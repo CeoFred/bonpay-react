c9cd57efa5dedae370df8f4ceb484f2641066c33
codemon <user@codemon-4.local>
2022-10-05 19:30:02 +0100

    f

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -6,7 +6,7 @@ import { useBonPay } from 'bonpay-react'
 const App = () => {
 
   const bondPay = useBonPay({ value: '0.006', recepient: '0xAFcC4d55a83ae1A449Bee7783A2737aFb5d82254', chainId: 97 });
-
+   
   function onSuccess(data: any) {
     console.log(data)
   }
